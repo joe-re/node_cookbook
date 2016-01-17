631b4101bd8a4e7c45d77716969c7f9499cd8aae
joe-re <joe.tialtngo@gmail.com>
2016-01-18 03:43:59 +0900

    set routing

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const http = require('http');
-http.createServer((_request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('woohoo!');
+const path = require('path');
+
+const pages = [
+  { route: '', output: 'Woohoo!' },
+  { route: 'about', output: 'This is simply sample code.' },
+  { route: 'another page', output: (route) => `This is ${route}` }
+];
+
+http.createServer((request, response) => {
+  const lookup = path.basename(decodeURI(request.url));
+  pages.forEach((page) => {
+    if (page.route === lookup) {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end(typeof page.output === 'function' ? page.output(page.route) : page.output);
+    }
+  });
+  if (!response.finished) {
+    response.writeHead(404);
+    response.end('ページが見つかりません!');
+  }
 }).listen(8080);
