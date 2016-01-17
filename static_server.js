2baca94fb719311e47f10c5f2766d257597ad934
joe-re <joe.tialtngo@gmail.com>
2016-01-18 06:02:54 +0900

    improve performance by using stream

--- static_server.js
+++ static_server.js
@@ -9,38 +9,32 @@ const mimeType = {
 };
 
 const cache = {};
-cacheAndDeliver = (f, cb) => {
-  fs.stat(f, (_err, stats) => {
-    const lastChanged = Date.parse(stats.ctime);
-    const isUpdated = (cache[f] && lastChanged > cache[f].timestamp);
-    if (!cache[f] || isUpdated) {
-      fs.readFile(f, (err, data) => {
-        if (!err) {
-          cache[f] = { content: data, timestamp: Date.now() };
-        }
-        cb(err, data);
-      });
-      return;
-    }
-    console.log(`load ${f} from cache.`);
-    cb(null, cache[f].content);
-  });
-};
 
 http.createServer((request, response) => {
   const lookup = path.basename(decodeURI(request.url)) || 'index.html';
   const f = 'content/' + lookup;
   fs.exists(f, (exists) => {
     if (exists) {
-      cacheAndDeliver(f, (err, data) => {
-        if (err) {
-          response.writeHead(500);
-          response.end('Server Error!');
-          return;
-        }
-        const headers = { 'Content-Type': mimeType[path.extname(f)] };
+      const headers = { 'Content-Type': mimeType[path.extname(f)] };
+      if (cache[f]) {
         response.writeHead(200, headers);
-        response.end(data);
+        response.end(cache[f].content);
+        return;
+      }
+      const s = fs.createReadStream(f).once('open', () => {
+        fs.stat(f, (_err, stats) => {
+          const bufferOffset = 0;
+          cache[f] = { content: new Buffer(stats.size) };
+          s.on('data', (data) => {
+            data.copy(cache[f].content, bufferOffset);
+            bufferOffset = data.length;
+          });
+        });
+        response.writeHead(200, headers);
+        s.pipe(response);
+      }).once('error', (e) => {
+        console.log(e);
+        console.log('Server Error!');
       });
       return;
     }
