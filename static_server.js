f9906a02af6f2742782fdfda3b233e5a1e3420e3
joe-re <joe.tialtngo@gmail.com>
2016-01-18 05:21:19 +0900

    caching logic for static file delivery

--- static_server.js
+++ static_server.js
@@ -8,12 +8,27 @@ const mimeType = {
   '.css': 'text/css'
 };
 
+const cache = {};
+cacheAndDeliver = (f, cb) => {
+  if (!cache[f]) {
+    fs.readFile(f, (err, data) => {
+      if (!err) {
+        cache[f] = { content: data };
+      }
+      cb(err, data);
+    });
+    return;
+  }
+  console.log(`load ${f} from chache.`);
+  cb(null, cache[f].content);
+};
+
 http.createServer((request, response) => {
   const lookup = path.basename(decodeURI(request.url)) || 'index.html';
   const f = 'content/' + lookup;
   fs.exists(f, (exists) => {
     if (exists) {
-      fs.readFile(f, (err, data) => {
+      cacheAndDeliver(f, (err, data) => {
         if (err) {
           response.writeHead(500);
           response.end('Server Error!');
